ea2f215df591e7eb77a0c8d5a16b1747de923fa9
halitklyc <halitenes.kalayci@gmail.com>
2025-08-26 11:33:56 +0300

    .

--- day2/car.py
+++ day2/car.py
@@ -1,19 +1,35 @@
 class Car(): # sisteme yeni bi obje tanıttım.
-    brand = ""
-    model = ""
-    year = 0
-    color = ""
+    def __init__(self, brand, model, year, color):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.color = color
+        print("abc123")
 
     def start_rent(self): 
         # self parametresi classın kendisini temsil eder. fonk. class seviyesine ulaşım sağlar
-        print(self.brand + " marka aracın kiralaması başlatılıyor.")
+        print(f"{self.brand} marka {self.model} modelindeki araç kiralaması başlatılıyor.")
+        #print(self.color + " rengindeki araç." + self.year + " yıl modeli.")
+        self.example()
+    def example(self):
+        print("Example çağırıldı.")
 
+# Eğer kalıtım varsa
+# Kalıtım alınan classa (Car) superclass
+# Kalıtım alan classa (ElectricCar) subclass denir.
+class ElectricCar(Car): #Car'daki bütün özellikler bu classda da vardır.
+    def __init__(self, brand, model, year, color, battery):
+        super().__init__(brand, model, year, color)
+        self.battery = battery
+        
+    def charge(self):
+        print("Araç şarj ediliyor..")
 
 # instance - örnek
-car1 = Car() # ilgili nesneden yeni bir üretim yaptım.
-car1.brand = "Toyota" # set
-car1.model = "Corolla" # set
-car1.color = "Kırmızı" # set
-car1.year = 2025 # set
+car1 = Car("Toyota","Corolla",2025, "Kırmızı") # ilgili nesneden yeni bir üretim yaptım.
 print(car1.year) # get
-car1.start_rent()
+car1.start_rent() # fonk.
+
+car2 = Car("Fiat","Egea",2024,"Beyaz") # nesneyi üretmek -> nesnenin __init__() çağırmak demek
+print(car2.year)
+car2.start_rent()

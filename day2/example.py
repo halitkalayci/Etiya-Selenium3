acf7ba92ba0685dbe03860cbfa2f12d6f545f4fa
halitklyc <halitenes.kalayci@gmail.com>
2025-08-26 14:36:47 +0300

    .

--- day2/example.py
+++ day2/example.py
@@ -15,8 +15,27 @@ def login_successfull():
     waiter.until(expected_conditions.visibility_of_element_located((By.ID,"password"))).send_keys("secret_sauce")
 
     waiter.until(expected_conditions.element_to_be_clickable((By.ID, "login-button"))).click()
+    print(driver.current_url == "https://www.saucedemo.com/inventory.html")
+    driver.quit()
+
+def login_unsuccessful():
+    driver = Chrome()
+    driver.maximize_window()
+    waiter = WebDriverWait(driver, 10) 
+    driver.get("https://www.saucedemo.com/")
+    username_input = waiter.until(expected_conditions.visibility_of_element_located((By.ID, "user-name")))     #driver.find_element(By.ID, "user-name") #locate
+    username_input.send_keys("standard_user")
+
+    waiter.until(expected_conditions.visibility_of_element_located((By.ID,"password"))).send_keys("secret_sauce123")
+
+    waiter.until(expected_conditions.element_to_be_clickable((By.ID, "login-button"))).click()
+    error_h3 = waiter.until(expected_conditions.visibility_of_element_located((By.XPATH, "//*[@id='login_button_container']/div/form/div[3]/h3")))
+    print(error_h3.text == "Epic sadface: Username and password do not match any user in this service")
+    driver.quit()
+
 
 login_successfull()
+login_unsuccessful()
 time.sleep(30)
 
 

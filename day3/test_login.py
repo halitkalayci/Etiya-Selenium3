2873c250214a11fb9232efd527e2e8b39b132dd9
halitklyc <halitenes.kalayci@gmail.com>
2025-08-27 11:13:04 +0300

    .

--- day3/test_login.py
+++ day3/test_login.py
@@ -3,30 +3,32 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 import pytest
-
+# 11:00
 class TestLogin():
-    @pytest.mark.skip
-    def test_success(self):
-        #Arrrange
+    @pytest.fixture #Bu methodun testlerin öncesi ve sonrasını yönetebilmesini sağlar.
+    def driver(self):
         driver = Chrome()
         driver.maximize_window()
+        yield driver
+        driver.quit()
+
+    @pytest.fixture
+    def waiter(self,driver):
         waiter = WebDriverWait(driver, 10)
-        #
-        #Act
+        yield waiter
+
+    #@pytest.mark.skip(reason="Bu sprintte devre dışı bırakıldı.")
+    def test_success(self, driver, waiter):
         driver.get("https://www.saucedemo.com/")
         username_input = waiter.until(expected_conditions.visibility_of_element_located((By.ID, "user-name")))   
         username_input.send_keys("standard_user")
         waiter.until(expected_conditions.visibility_of_element_located((By.ID,"password"))).send_keys("secret_sauce")
         waiter.until(expected_conditions.element_to_be_clickable((By.ID, "login-button"))).click()
-        #Assert
         assert driver.current_url == "https://www.saucedemo.com/inventory.html"
     
     # Decorator -> Fonksiyonun içeriğini düzenlemeden çalışma mantığını genişletmek/düzenlemek.
     @pytest.mark.parametrize("username,password", [ ("abc123","abc123"), ("myuser","mypassword"), ("123","123") ])
-    def test_invalid_user(self, username, password):
-        driver = Chrome()
-        driver.maximize_window()
-        waiter = WebDriverWait(driver, 10) 
+    def test_invalid_user(self, driver, waiter, username, password):
         driver.get("https://www.saucedemo.com/")
         username_input = waiter.until(expected_conditions.visibility_of_element_located((By.ID, "user-name")))  
         username_input.send_keys(username)
@@ -34,4 +36,3 @@ class TestLogin():
         waiter.until(expected_conditions.element_to_be_clickable((By.ID, "login-button"))).click()
         error_h3 = waiter.until(expected_conditions.visibility_of_element_located((By.XPATH, "//*[@id='login_button_container']/div/form/div[3]/h3")))
         assert error_h3.text == "Epic sadface: Username and password do not match any user in this service"
-        

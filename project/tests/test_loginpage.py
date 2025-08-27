7967a92c164caa78c8e0d198c3d939b643e1dc24
halitklyc <halitenes.kalayci@gmail.com>
2025-08-27 15:12:10 +0300

    .

--- project/tests/test_loginpage.py
+++ project/tests/test_loginpage.py
@@ -1,14 +1,17 @@
 from pages.login_page import LoginPage
+from utils.image_helper import save_screenshot
 
 class TestLogin():
     def test_successfull(self,driver,waiter):
         page = LoginPage(driver,waiter)
         page.load()
         page.login("standard_user","secret_sauce")
+        save_screenshot(driver, "test_successfull")
         assert driver.current_url == "https://www.saucedemo.com/inventory.html"
     
     def test_invalid_user(self, driver, waiter):
         page = LoginPage(driver,waiter)
         page.load()
         page.login("standard_user","secret_sauce123")
+        save_screenshot(driver, "test_invalid_user")
         assert page.get_error_message() == "Epic sadface: Username and password do not match any user in this service" 
--- project/utils/image_helper.py
+++ project/utils/image_helper.py
@@ -5,11 +5,9 @@ def save_screenshot(driver:Chrome, name="screenshot"):
     screenshot_folder = "C:\\Tests\\Screenshots\\" + datetime.now().strftime("%d-%m-%Y")
     os.makedirs(screenshot_folder, exist_ok=True)
 
-    file_name = name + "_" + datetime.now().strftime("%H-%M-%S") + "_" + datetime.now().microsecond + ".png"
+    file_name = f"{name}_{datetime.now().strftime("%H-%M-%S")}_{datetime.now().microsecond}.png"
 
     abs_path = screenshot_folder+"\\"+file_name
     driver.save_screenshot(abs_path)
 
     return abs_path
-
-# 15:05

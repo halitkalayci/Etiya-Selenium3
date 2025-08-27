66c189274a1119544ff678713d0e85b01ba4d892
halitklyc <halitenes.kalayci@gmail.com>
2025-08-27 14:24:46 +0300

    ..

--- project/pages/login_page.py
+++ project/pages/login_page.py
@@ -10,6 +10,7 @@ class LoginPage():
         self.username_input = (By.ID, "user-name")
         self.password_input = (By.ID, "password")
         self.login_button = (By.ID, "login-button")
+        self.error_container = (By.CSS_SELECTOR, "[data-test='error']")
 
     def load(self):
         self.driver.get("https://saucedemo.com")
@@ -20,3 +21,5 @@ class LoginPage():
         self.wait.until(expected_conditions.visibility_of_element_located(self.password_input)).send_keys(password)
         self.wait.until(expected_conditions.visibility_of_element_located(self.login_button)).click()
 
+    def get_error_message(self):
+        return self.wait.until(expected_conditions.visibility_of_element_located(self.error_container)).text
--- project/tests/test_loginpage.py
+++ project/tests/test_loginpage.py
@@ -6,3 +6,9 @@ class TestLogin():
         page.load()
         page.login("standard_user","secret_sauce")
         assert driver.current_url == "https://www.saucedemo.com/inventory.html"
+    
+    def test_invalid_user(self, driver, waiter):
+        page = LoginPage(driver,waiter)
+        page.load()
+        page.login("standard_user","secret_sauce123")
+        assert page.get_error_message() == "Epic sadface: Username and password do not match any user in this service" 
